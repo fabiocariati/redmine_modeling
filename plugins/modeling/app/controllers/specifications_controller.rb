adfae88b196ba09f55f33c940e7fec1d193a48e3
fabiodoctorsys <fabiocariati@gmail.com>
2014-05-06 23:27:36 -0300

    heroku5

--- plugins/modeling/app/controllers/specifications_controller.rb
+++ plugins/modeling/app/controllers/specifications_controller.rb
@@ -4,7 +4,7 @@ class SpecificationsController < ApplicationController
   def index
     #Dir.mkdir 'log/test'
     #out_file = File.new("log/test/out.txt", "w")
-    out_file = File.new("log/out.txt", "w")
+    out_file = File.new("#{Rails.root}/log/out.txt", "w")
     @project = Project.find(params[:project_id])
     @specifications = Specification.where(project_id: @project.id)
 
